2aba92723da1c4301ce3441c33d6a228ea994a3a
Marcin Korwek <marcin.korwek4@gmail.com>
2024-09-18 11:31:09 +0200

    configure gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === "production" ? "/pomodoro/" : "/"
 })
